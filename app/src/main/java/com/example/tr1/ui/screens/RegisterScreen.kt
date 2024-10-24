25be46cd2684b5dce0fd5e525f73efbcfd7ede24
a22arnmaljoa <a22arnmaljoa@inspedralbes.cat>
2024-10-24 12:53:03 +0200

    Buttons mail i confirmpassword afegits

--- app/src/main/java/com/example/tr1/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/tr1/ui/screens/RegisterScreen.kt
@@ -13,7 +13,9 @@ import com.example.tr1.ui.TakeAwayApp
 @Composable
 fun RegisterScreen(navController: NavHostController) {
     var username by remember { mutableStateOf("") }
+    var mail by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var confirmpassword by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -38,9 +40,31 @@ fun RegisterScreen(navController: NavHostController) {
         Spacer(modifier = Modifier.height(16.dp))
 
         TextField(
+            value = mail,
+            onValueChange = { mail = it },
+            label = { Text(text = "Correo") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
             value = password,
             onValueChange = { password = it },
             label = { Text(text = "Contraseña") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = confirmpassword,
+            onValueChange = { confirmpassword = it },
+            label = { Text(text = "Confirmar contrasenya") },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 15.dp)
